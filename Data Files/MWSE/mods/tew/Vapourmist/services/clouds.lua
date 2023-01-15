9715ec2edafe04be79e88c6bf48283a95627604b
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-01-15 22:38:36 +0100

    Bump timer

--- Data Files/MWSE/mods/tew/Vapourmist/services/clouds.lua
+++ Data Files/MWSE/mods/tew/Vapourmist/services/clouds.lua
@@ -10,7 +10,7 @@ local config = require("tew.Vapourmist.config")
 -->>>---------------------------------------------------------------------------------------------<<<--
 -- Constants
 
-local TIMER_DURATION = 0.2
+local TIMER_DURATION = 0.25
 
 local CELL_SIZE = 8192
 
